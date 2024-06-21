7b7cb5a149645647d2fea0af9de7a0331f4dd895
coskar <oskar.cokl@iprom.si>
2024-06-21 12:41:46 +0200

    feat: Add styling to page.

--- index.php
+++ index.php
@@ -11,43 +11,46 @@
 </head>
 
 <body>
-    <div class="container">
-        <div>
-            <form class="row g-3 d-flex justify-content-center align-items-end" action="index.php" method="post">
-                <div class="col-4">
-                    <label for="feedUrl" class="form-label">Input RSS feed url</label>
-                    <input type="text" id="feedUrl" name="feedUrl" class="form-control">
-                </div>
-                <div class="col-2">
-                    <button class="btn btn-primary text-uppercase" type="submit">get feed</button>
-                </div>
-            </form>
+    <div class="container mt-4">
+        <div class="row">
+            <h1 class="text-center">RSS feed reader</h1>
         </div>
-        <div>
-            <?php
-            require 'vendor/autoload.php';
+        <form class="row d-flex justify-content-center align-items-end" action="index.php" method="post">
+            <div class="col-4">
+                <label for="feedUrl" class="form-label">Input RSS feed url</label>
+                <input type="text" id="feedUrl" name="feedUrl" class="form-control">
+            </div>
+            <div class="col-2">
+                <button class="btn btn-primary text-uppercase" type="submit">get feed</button>
+            </div>
+        </form>
+        <!-- Load rss feed -->
+        <?php
+        require 'vendor/autoload.php';
 
-            $url = isset($_POST['feedUrl']) ? $_POST['feedUrl'] : '';
-            if (!$url) return;
+        $url = isset($_POST['feedUrl']) ? $_POST['feedUrl'] : '';
+        if (!$url) return;
 
-            $rss = Feed::loadRss($url);
-            ?>
+        $rss = Feed::loadRss($url);
+        ?>
+        <div class="row d-flex justify-content-center mt-4">
+            <div class="col-7">
+                <h4><?php echo 'Title: ', $rss->title; ?></h4>
+                <h6><?php echo 'Link: ', $rss->link; ?></h6>
+                <p><?php echo 'Description: ', $rss->description; ?></p>
+            </div>
+
         </div>
-        <div>
-            <?php
-            echo 'Title: ', $rss->title;
-            echo 'Description: ', $rss->description;
-            echo 'Link: ', $rss->url;
-            ?>
-        </div>
-        <div>
+        <div class="row d-flex justify-content-center">
             <?php
             foreach ($rss->item as $item) {
-                echo 'Title: ', $item->title;
-                echo 'Link: ', $item->url;
-                echo 'Timestamp: ', $item->timestamp;
-                echo 'Description: ', $item->description;
-                echo 'HTML encoded content: ', $item->{'content:encoded'};
+                echo '<div class="card d-flex justify-content-center mb-3" style="width: 50rem">';
+                echo '<div class="card-body">';
+                echo '<h4 class="card-title "><a class="link-underline link-underline-opacity-0 text-primary" href="' . $item->link .'">' . $item->title . '</a></h4>';
+                echo '<h6 class="card-subtitle text-body-secondary">Date: ', date('d. M. Y', (int)$item->timestamp) . '</h6>';
+                echo '<p class="fs-6">Description: ', $item->description . '</p>';
+                echo '</div>';
+                echo '</div>';
             }
             ?>
         </div>
